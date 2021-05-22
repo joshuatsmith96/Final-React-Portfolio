2081bbfcdea50dc7f50e0fc7910f3378003579cf
joshuatsmith96 <joshuatsmith96@gmail.com>
2021-05-21 21:07:24 -0500

    Added Routing and Redirect
    If a user tries to navigate to another page in the URL, it will default back to the home page.

--- final-react-portfolio/src/App.js
+++ final-react-portfolio/src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
 import Home from './component/Sections/Home/Home';
 import Loading from './component/Sections/Loading/Loading';
 
 function App() {
-  return (
-    <div className="App">
+  return <Router>
+  <Switch>
+    <Route path="/" exact>
       <Home />
-      {isLoading && <Loading />}
-    </div>
-  );
+    </Route>
+    <Redirect to="/" />
+  </Switch>
+
+</Router>;
 }
 
 export default App;
